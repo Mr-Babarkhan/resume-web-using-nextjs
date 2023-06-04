26d056dcd8b74970f4e101f72f382ce78b2b45ab
Mr-Babarkhan <muhammadbabarkhan813@gmail.com>
2023-06-04 06:34:18 -0700

    first commit

---
+++ data/page-data.jsx
@@ -0,0 +1,66 @@
+export const personalData = {
+	name: "Babar khan",
+	role: "Software Engineer",
+	education: [
+		"Bachelor of Science in software engineering, University of Haripur(2020-present)",
+		"Certified web 3.0 and Metaverse developer (2022-present)",
+	],
+	contactLinks: [
+		"mrkb3065@gmail.com",
+		"",
+		"https://www.linkedin.com/in/babar-khan-42b729235",
+		"",
+	],
+};
+
+export const aboutMe = {
+	title: "About Me",
+	body: [
+		"As a highly motivated and driven individual with a passion for making a positive impact, I have a unique blend of technical and interpersonal skills, thanks to my background in Software engineering. I am able to tackle complex problems with ease, and I never lose sight of my ultimate goal, even when facing challenges.",
+		"When I'm not working, I love hiking, reading, and spending time with my friends and family. I believe that my determination, can-do attitude, and hard work make me a true asset to any team.",
+	],
+};
+
+export const skills = {
+	soft: [
+		{ icon: "👂🏼", text: "Active Listening" },
+		{ icon: "💬", text: "Effective Communication" },
+		{ icon: "👥", text: "Collaboration" },
+		{ icon: "⽓", text: "Teamwork" },
+		{ icon: "💡", text: "Creative Problem Solving" },
+		{ icon: "⌛️", text: "Time management" },
+	],
+	hard: [
+		{ icon: "💻", text: "Next js" },
+		{ icon: "ʦ", text: "TypeScript" },
+		{ icon: "🚀", text: "React" },
+		{ icon: "💾", text: "SQL" },
+		{ icon: "💾", text: "Node js" },
+		{ icon: "📈", text: "UI and UX" },
+	],
+};
+
+export const professionalData = {
+	title: "Professional Experience",
+	experiences: [
+		{
+			role: "Web Developer. (2020-Present)",
+			description:
+				"I have worked with various technologies to create robust and scalable applications. I am proficient in Next.js, a React-based framework for building server-side rendered applications, as well as Tailwind CSS, a utility-first CSS framework that allows for fast and efficient styling. I am also skilled in HTML, JavaScript, and TypeScript, which are essential for creating dynamic and interactive web interfaces.",
+			current: true,
+		},
+
+		{
+			role: "Web 3.0 and Metaverse development (2022-present)",
+			description:
+				"Since 2022, I have been actively involved in Web 3.0 and metaverse development. During this time, I have been engaged in building decentralized applications (DApps) that leverage blockchain technology to provide enhanced security and user control over their data. Additionally, I have explored the integration of virtual reality (VR) elements into these applications, creating immersive and interactive experiences within the metaverse. By staying at the forefront of emerging technologies, I have been able to contribute to the growth and advancement of Web 3.0 and metaverse development, shaping the future of the internet and virtual experiences.",
+			current: false,
+		},
+		{
+			role: "Graphic designing (2020-present)",
+			description:
+				"In graphic designing, I have been actively involved since 2020, honing my skills and expertise in creating visually captivating designs. I have worked on various projects, collaborating closely with clients to understand their requirements and translate their ideas into compelling visuals.",
+			current: false,
+		},
+	],
+};
